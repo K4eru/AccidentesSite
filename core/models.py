c764ce5923e52e792ba6ed1d06be19ce782a4f15
Z3pphyr <maxi.lopez.pereira@gmail.com>
2021-10-06 22:25:00 -0300

    Custom user y html base creado

--- core/models.py
+++ core/models.py
@@ -1,3 +1,20 @@
-from django.db import models
+from typing import AbstractSet
+from django.db import models 
+from django.contrib.auth.models import User
 
-# Create your models here.
+userType=(
+    (0,'Admin'),
+    (1,'Cliente'),
+    (2,'Profesional')
+         )
+
+class userModel(models.Model):
+    userRut= models.CharField(max_length=30 , unique= True)
+    userType= models.IntegerField(choices=userType)
+
+    user = models.OneToOneField(User, on_delete = models.CASCADE, related_name="extend")
+
+    def __str__(self):
+        return self.userRut
+
+
